0c2b10753b29ff8e800826cc3320ba9138deb032
Andrew Jones <ximenean@hotmail.com>
2021-12-31 22:41:05 +0000

    Update to auto-install packer

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,3 +1,17 @@
+local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = vim.fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packeradd packer.nvim]]
+end
+
 local packer = require('packer')
 
 -- Autocommand that reloads neovim whenever you save this file
